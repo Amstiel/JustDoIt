24c56e96eb6877ead7cf485a0a4889a2ef40608d
Alexey Kunitsky <anonymouss.alex@gmail.com>
2016-03-23 17:27:35 +1000

    Add Dialog
    Add simple dialog, delete some shit

--- app/src/main/java/com/example/aleksey/justdoit/DoListElement.java
+++
@@ -1,7 +0,0 @@
-package com.example.aleksey.justdoit;
-
-/**
- * Created by Aleksey on 21.03.2016.
- */
-public class DoListElement {
-}
--- app/src/main/java/com/example/aleksey/justdoit/MainActivity.java
+++ app/src/main/java/com/example/aleksey/justdoit/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.aleksey.justdoit;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    String message;
+    AlertDialog.Builder alert;
+    TextView tw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        alert = new AlertDialog.Builder(this);
+        tw = (TextView) findViewById(R.id.textView);
+    }
+
+
+    public void addBtnClick(View view) {
+        final EditText edittext = new EditText(MainActivity.this);
+        alert.setTitle("Add ToDo item, please");
+
+        alert.setView(edittext);
+
+        alert.setPositiveButton("Enter", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                message = edittext.getText().toString();
+                tw.setText(message.toString()); //Just a testing shit
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+            }
+        });
+        alert.show();
     }
 }
