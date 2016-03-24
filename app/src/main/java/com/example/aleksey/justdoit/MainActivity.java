8cd5a92b55f28978e73af97dfd20457b07d0c70e
Alexey Kunitsky <anonymouss.alex@gmail.com>
2016-03-24 14:05:28 +1000

    Add fab. Add russian localization.
    Add FloatingActionButton. Add russian localization to strings.

--- app/src/main/java/com/example/aleksey/justdoit/MainActivity.java
+++ app/src/main/java/com/example/aleksey/justdoit/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.aleksey.justdoit;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,16 +18,15 @@ import android.widget.TextView;
 
 import com.firebase.client.Firebase;
 import com.firebase.ui.FirebaseListAdapter;
+import com.melnykov.fab.FloatingActionButton;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView lv;
-    String message;
-    AlertDialog.Builder alert;
-    AlertDialog.Builder alert1;
-    TextView tw;
+    AlertDialog.Builder alertAdd;
+    AlertDialog.Builder alertDelete;
     Firebase mRef;
     FirebaseListAdapter<String> fireAdapter;
 
@@ -41,55 +42,63 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void populateView(View view, String s, int i) {
                 TextView text = (TextView)view.findViewById(android.R.id.text1);
-                text.setText(s + i);
+                text.setText(s);
             }
         };
-        alert1 = new AlertDialog.Builder(this);
-        alert = new AlertDialog.Builder(this);
-        tw = (TextView) findViewById(R.id.textView);
+
+        alertAdd = new AlertDialog.Builder(this);
+        alertDelete = new AlertDialog.Builder(this);
+
         lv.setAdapter(fireAdapter);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-                alert1.setTitle("Are you want to delete this item?");
+                alertDelete.setTitle(R.string.delete_item_message);
 
-                alert1.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                alertDelete.setPositiveButton(R.string.delete_yes_buttom, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
                         Firebase tmp = fireAdapter.getRef(position);
                         tmp.removeValue();
                     }
                 });
 
-                alert1.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                alertDelete.setNegativeButton(R.string.delete_no_buttom, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
                     }
                 });
-                alert1.show();
+                alertDelete.show();
             }
         });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.attachToListView(lv);
+
+        View.OnClickListener listener= new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText edittext = new EditText(MainActivity.this);
+                alertAdd.setTitle(R.string.add_item_message);
+
+                alertAdd.setView(edittext);
+
+                alertAdd.setPositiveButton(R.string.add_yes_buttom, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        if (!edittext.getText().toString().isEmpty()) {
+                            addItem(edittext.getText().toString());
+                        }
+                    }
+                });
+
+                alertAdd.setNegativeButton(R.string.add_no_buttom, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                });
+                alertAdd.show();
+            }
+        };
+        fab.setOnClickListener(listener);
     }
 
     public void addItem(String item){
         mRef.push().setValue(item);
-    }
-
-    public void addBtnClick(View view) {
-        final EditText edittext = new EditText(MainActivity.this);
-        alert.setTitle("Add ToDo item, please");
-
-        alert.setView(edittext);
-
-        alert.setPositiveButton("Enter", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                if (!edittext.getText().toString().isEmpty()) {
-                    addItem(edittext.getText().toString());
-                }
-            }
-        });
-
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-            }
-        });
-        alert.show();
     }
 }
